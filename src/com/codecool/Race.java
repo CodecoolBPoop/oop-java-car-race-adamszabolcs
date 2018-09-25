4aaa405b2cee94ec70aa4a87f73654ae5abe3937
adamszabolcs <szabolcs.adam91@gmail.com>
2018-09-25 14:03:18 +0200

    implemented Race methods

--- src/com/codecool/Race.java
+++ src/com/codecool/Race.java
@@ -5,28 +5,48 @@ import java.util.List;
 
 public class Race {
 
+    private int fullRace = 50;
+
+    private List<Truck> trucks = new ArrayList<>();
+
+    private List<Motorcycle> motorcycles = new ArrayList<>();
+
     private List<Car> cars = new ArrayList<>();
 
     public static final int racerNumber = 10;
 
     public Race() {
-        createVehicles();
     }
 
     void createVehicles() {
         for (int i = 0; i < racerNumber; i++) {
             Car car = new Car();
+            Truck truck = new Truck();
+            Motorcycle motor = new Motorcycle();
             cars.add(car);
+            trucks.add(truck);
+            motorcycles.add(motor);
         }
-        for (int i = 0; i < 2; i++) {
-            System.out.println(Weather.checkIfRaining());
+    }
+
+    void simulateRace() {
+        createVehicles();
+        for (int i = 0; i < fullRace; i++) {
+            Weather.setRaining();
             for (Car car: cars) {
-                System.out.println(car.getName());
-                System.out.println(car.getNormalSpeed());
-                car.addDistanceTraveled();
-                System.out.println(car.getDistanceTraveled());
+                car.moveForAnHour();
+            }
+            for (Truck truck : trucks) {
+                truck.moveForAnHour();
+            }
+            for (Motorcycle motor : motorcycles) {
+                motor.moveForAnHour();
             }
         }
+        printRaceResults();
+    }
+
+    void printRaceResults() {
         int maxDistance = 0;
         String winnerName = "";
         for (Car car : cars) {
@@ -39,18 +59,12 @@ public class Race {
         System.out.println(winnerName);
     }
 
-    void simulateRace() {
-    }
-
-    void printRaceResults() {
-
-    }
-
     boolean isThereABrokenTruck() {
         return false;
     }
 
     public static void main(String[] args) {
 	    Race race = new Race();
+	    race.simulateRace();
     }
 }
