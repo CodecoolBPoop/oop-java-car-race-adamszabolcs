a8214aa91d38b1b1e20960c1b93d8527cb5d01d1
adamszabolcs <szabolcs.adam91@gmail.com>
2018-09-25 10:06:57 +0200

    Started implementation of Motorcycle class

--- src/com/codecool/Motorcycle.java
+++ src/com/codecool/Motorcycle.java
@@ -2,10 +2,14 @@ package com.codecool;
 
 public class Motorcycle {
 
+    public Motorcycle() {
+        speed = 100;
+    }
+
     int speed;
     static String nameNumber;
     int distanceTraveled;
-    void moveForAnHour() {
+    void moveForAnHour(Race race) {
 
     }
 }
