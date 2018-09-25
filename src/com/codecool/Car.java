e68baed1daff2f3291676d3d5156ecc503fc4568
adamszabolcs <szabolcs.adam91@gmail.com>
2018-09-25 12:53:38 +0200

    created Car constructor, create name, set speed, check if raining

--- src/com/codecool/Car.java
+++ src/com/codecool/Car.java
@@ -1,10 +1,71 @@
 package com.codecool;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Car {
+
+    private static final int carNameLength = 2;
+    private static final int rainingSpeed = 75;
+    public static final String[] carNames = {"Eon", "Crest", "Patron", "Blast", "Zeal", "Temper", "Crux", "Dynamics",
+                                            "Flow", "Ivory", "Fragment", "Tradition", "Viper", "Universe", "Escape",
+                                            "Magic", "Flux", "Blizzard"};
+
+    private String name;
+
+    private int normalSpeed;
+
+    private int distanceTraveled = 0;
+
+    private static String randomName = "";
+
     public Car() {
-        int normalSpeed;
-        String name;
-        int distanceTraveled;
+        createName();
+        createNormalSpeed();
+    }
+
+    void makeDistance(int normalSpeed) {
+        distanceTraveled += normalSpeed;
+    }
+
+    int getDistanceTraveled() {
+        return distanceTraveled;
+    }
+
+    void createNormalSpeed() {
+        if (Weather.isRaining) {
+            setNormalSpeed(rainingSpeed);
+        } else {
+            Random randomSpeed = new Random();
+            setNormalSpeed((randomSpeed.nextInt(30) + 1) + 80);
+        }
+    }
+
+    void setNormalSpeed(int normalSpeed) {
+        this.normalSpeed = normalSpeed;
+    }
+
+    int getNormalSpeed() {
+        createNormalSpeed();
+        return normalSpeed;
+    }
+
+    void setName(String name) {
+        this.name = name;
+    }
+
+    String getName() {
+        return name;
+    }
+
+    void createName() {
+        randomName = "";
+        for (int j = 0; j < carNameLength; j++) {
+            Random carNameIndex = new Random();
+            randomName += carNames[carNameIndex.nextInt(carNames.length - 1)] + " ";
+        }
+        setName(randomName);
     }
 
     void moveForAnHour(Race race) {
